cd0e6f03b1a3931f456bb5cff45910312b2d7b3d
Julian Wenzel <j.w-a@t-online.de>
2024-11-14 17:14:03 +0100

    first commit

---
+++ first_program.py
@@ -0,0 +1,5 @@
+print (10 * (3 + 2))
+print (10/4)
+print(50 + 6 * 7)
+print (2**3)
+print ("Hello")
---
+++ main.py
@@ -0,0 +1,8 @@
+# EPython: Main procedure
+
+
+# Add 1 and 2
+print (1 + 2)  
+
+# Use Python as a complex pocket calculator
+x = 8 * ((3 + 2) / 5) ** 2
