4223ede17c41a9e9273a6cb17edaae2249ae1830
Julian Wenzel <j.w-a@t-online.de>
2024-11-14 17:38:17 +0100

    Video 2 Abgeschlossen (Einführung IDE)

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ print (1 + 2)
 
 # Use Python as a complex pocket calculator
 x = 8 * ((3 + 2) / 5) ** 2
+
+# What about about a Input
+#name = input("Name: ")
+#print (name)
+
+value = "False" * 3
+print (value)
